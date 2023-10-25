03fb9632a5ac4bb9c935b7b50b41df72f10d0c2e
HuHu <hongbo.hhu318@gmail.com>
2023-10-24 21:37:01 -0400

    add frontend interface, add(fix) login function

---
+++ src/services/humbleapi-backend/interfaceInfoController.ts
@@ -0,0 +1,48 @@
+// @ts-ignore
+/* eslint-disable */
+import { request } from '@umijs/max';
+
+/** addInterfaceInfo POST /api/interfaceInfo/add */
+export async function addInterfaceInfoUsingPOST(
+  body: API.InterfaceInfoAddRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseLong_>('/api/interfaceInfo/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** deleteInterfaceInfo POST /api/interfaceInfo/delete */
+export async function deleteInterfaceInfoUsingPOST(
+  body: API.DeleteRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseBoolean_>('/api/interfaceInfo/delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** updateInterfaceInfo POST /api/interfaceInfo/update */
+export async function updateInterfaceInfoUsingPOST(
+  body: API.InterfaceInfoUpdateRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseBoolean_>('/api/interfaceInfo/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
